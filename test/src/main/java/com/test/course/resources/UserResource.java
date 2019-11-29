4795aaa5c39db73354b29a6492f4e7d60dff548e
Guilherme Castro <guilhermecastro.developer@gmail.com>
2019-11-29 00:00:27 -0300

    Project created

---
+++ test/src/main/java/com/test/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.test.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.test.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Guilherme", "guilherme@gmail.com", "+5521976728011", "1234567g");
+		return ResponseEntity.ok().body(u);
+	}
+}
